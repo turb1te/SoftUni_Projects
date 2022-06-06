d12ab678e4a368a98ead665c24631d82263ea2c9
Martin Kirekchiev <sumoray35@gmail.com>
2022-06-06 16:56:38 +0300

    Upload Print_different_types_of_variables.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

---
+++ src/FirstStepsInCoding/Lab/Print_different_types_of_variables.java
@@ -0,0 +1,16 @@
+package FirstStepsInCoding.Lab;
+
+public class Print_different_types_of_variables {
+    public static void main(String[] args) {
+        // VARIABLES
+        String text = "Lorem ipsum";
+        int Integer = 2022;
+        double Fraction = 20.22;
+        char Symbol = '$';
+        // PRINT COMMANDS
+        System.out.println(text);
+        System.out.println(Integer);
+        System.out.println(Fraction);
+        System.out.println(Symbol);
+    }
+}
