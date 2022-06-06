40691959d0ee953d02306c71d5ee473f55491b29
Martin Kirekchiev <sumoray35@gmail.com>
2022-06-06 19:42:21 +0300

    Upload USD_to_BGN.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

---
+++ src/FirstStepsInCoding/Lab/USD_to_BGN.java
@@ -0,0 +1,14 @@
+package FirstStepsInCoding.Lab;
+
+import java.util.Scanner;
+
+public class USD_to_BGN {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+
+        double usd = Double.parseDouble(user_input.nextLine());
+        double bgn = usd * 1.79549;
+
+        System.out.printf("%.2f USD is equivalent to %.2f BGN!", usd, bgn);
+    }
+}
