744c2f2919f16fbeb6a6a4a5e60a51cae243804e
Martin Kirekchiev <sumoray35@gmail.com>
2022-06-06 19:46:19 +0300

    Upload Radians_to_degrees.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

---
+++ src/FirstStepsInCoding/Lab/Radians_to_degrees.java
@@ -0,0 +1,14 @@
+package FirstStepsInCoding.Lab;
+
+import java.util.Scanner;
+
+public class Radians_to_degrees {
+    public static void main(String[] args) {
+        Scanner user_input = new Scanner(System.in);
+
+        double radians = Double.parseDouble(user_input.nextLine());
+        double degrees = radians * 180 / Math.PI;
+
+        System.out.println(degrees);
+    }
+}
