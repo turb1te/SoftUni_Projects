b7a289d627f4343720d5f1c9ac8ca8e9486c87e3
Martin Kirekchiev <sumoray35@gmail.com>
2022-06-28 21:22:46 +0300

    Update LessBetweenGreater.java
    Simplify the first else-if.
    
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

--- src/ConditionalStatements/Lab/LessBetweenGreater.java
+++ src/ConditionalStatements/Lab/LessBetweenGreater.java
@@ -10,7 +10,7 @@ public class LessBetweenGreater {
 
         if (num < 100) {
             System.out.println("Less than 100");
-        } else if (num >= 100 && num <= 200) {
+        } else if (num <= 200) {
             System.out.println("Between 100 and 200");
         } else {
             System.out.println("Greater than 200");
