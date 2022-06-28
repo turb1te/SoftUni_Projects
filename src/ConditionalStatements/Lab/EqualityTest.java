9b97d7f1ed7eb48ce69598e944759e516da536f5
Martin Kirekchiev <sumoray35@gmail.com>
2022-06-28 17:15:17 +0300

    Update EqualityTest.java
    The corrections were made with the help of Grammarly.
    
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

--- src/ConditionalStatements/Lab/EqualityTest.java
+++ src/ConditionalStatements/Lab/EqualityTest.java
@@ -6,10 +6,10 @@ public class EqualityTest {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-        String a = userInput.nextLine(); // We input "Martin" and expect return to be *false* because the strings have different addresses in memory because of scanner.
-        //String a = "Martin"; // We expect return to be *true* because the "a" and "b" strings share the same memory address.
+        String a = userInput.nextLine(); // We input "Martin" and expect the return to be *false* because the strings have different addresses in memory because of the scanner.
+        //String a = "Martin"; // We expect the return to be *true* because the "a" and "b" strings share the same memory address.
         String b = "Martin";
 
-        System.out.println(a == b); // The correct way to compare strings is to use equals() method not == operator.
+        System.out.println(a == b); // The correct way to compare strings is to use equals() method, not == operator.
     }
 }
