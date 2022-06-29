aeb19c0629ece8bb5fb29f93935dc138ce0a925b
Martin Kirekchiev <sumoray35@gmail.com>
2022-06-29 16:03:21 +0300

    Update ToyShop.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

--- src/ConditionalStatements/Exercises/ToyShop.java
+++ src/ConditionalStatements/Exercises/ToyShop.java
@@ -6,25 +6,27 @@ public class ToyShop {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-        double excursionPrice = Double.parseDouble(userInput.nextLine());
+        double tourPrice = Double.parseDouble(userInput.nextLine());
         int numOfPuzzles = Integer.parseInt(userInput.nextLine());
         int numOfTalkingDolls = Integer.parseInt(userInput.nextLine());
         int numOfTeddyBears = Integer.parseInt(userInput.nextLine());
         int numOfMinions = Integer.parseInt(userInput.nextLine());
         int numOfTrucks = Integer.parseInt(userInput.nextLine());
+        int totalCount = numOfPuzzles + numOfTalkingDolls + numOfTeddyBears + numOfMinions + numOfTrucks;
 
         double totalPrice = (numOfPuzzles * 2.60) + (numOfTalkingDolls * 3.00) + (numOfTeddyBears * 4.10) + (numOfMinions * 8.20) + (numOfTrucks * 2.00);
 
-        if (numOfPuzzles + numOfTalkingDolls + numOfTeddyBears + numOfMinions + numOfTrucks >= 50) {
+        if (totalCount >= 50) {
             totalPrice *= 0.75;
         }
 
-        double moneyAfterRent = totalPrice * 0.90;
+        double moneyLeft_afterRent = totalPrice * 0.90;
+        double moneyLeft_afterTour = moneyLeft_afterRent - tourPrice;
 
-        if (moneyAfterRent >= excursionPrice) {
-            System.out.printf("Yes! %.2f lv left.", moneyAfterRent - excursionPrice);
+        if (moneyLeft_afterRent >= tourPrice) {
+            System.out.printf("Yes! %.2f lv left.", moneyLeft_afterTour);
         } else {
-            System.out.printf("Not enough money! %.2f lv needed.", excursionPrice - moneyAfterRent);
+            System.out.printf("Not enough money! %.2f lv needed.", Math.abs(moneyLeft_afterTour));
         }
     }
 }
