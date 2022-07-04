e3b4cb293c576742b959e42aa82f6eaaacc03821
Martin Kirekchiev <sumoray35@gmail.com>
2022-07-04 17:23:32 +0300

    Update TheDaysOfTheWeek.java
    Use enchanted 'switch' statement. Recommended: Java 13+
    
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

--- src/ConditionalStatementsAdvanced/Lab/TheDaysOfTheWeek.java
+++ src/ConditionalStatementsAdvanced/Lab/TheDaysOfTheWeek.java
@@ -9,30 +9,14 @@ public class TheDaysOfTheWeek {
         int dayNumber = Integer.parseInt(userInput.nextLine());
 
         switch (dayNumber) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Error");
-                break;
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Error");
         }
     }
 }
