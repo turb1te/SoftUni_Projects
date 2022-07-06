a9c488a320521ddde73d5ae44afadf061aedb57c
Martin Kirekchiev <sumoray35@gmail.com>
2022-07-06 18:28:11 +0300

    Upload Numbers_From_1_to_100.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

---
+++ src/ForLoop/Lab/Numbers_From_1_to_100.java
@@ -0,0 +1,9 @@
+package ForLoop.Lab;
+
+public class Numbers_From_1_to_100 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
