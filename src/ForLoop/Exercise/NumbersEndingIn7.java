8c8c8f5b998d12e00070f2d9417d37336821d87e
Martin Kirekchiev <sumoray35@gmail.com>
2022-07-07 14:41:32 +0300

    Optimize NumbersEndingIn7.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

--- src/ForLoop/Exercise/NumbersEndingIn7.java
+++ src/ForLoop/Exercise/NumbersEndingIn7.java
@@ -6,11 +6,8 @@ public class NumbersEndingIn7 {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
 
-        for (int i = 0; i < 1000; i++) {
-            if (i % 10 == 7) {
-                System.out.println(i);
-            }
+        for (int i = 7; i <= 997; i += 10) {
+            System.out.println(i);
         }
-
     }
 }
