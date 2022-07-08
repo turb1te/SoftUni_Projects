27fd353e5c6dbc361e06af8d1cb139f78aaa8667
Martin Kirekchiev <sumoray35@gmail.com>
2022-07-08 16:00:07 +0300

    Upload Salary.java
    Signed-off-by: Martin Kirekchiev <sumoray35@gmail.com>

---
+++ src/ForLoop/Exercise/Salary.java
@@ -0,0 +1,38 @@
+package ForLoop.Exercise;
+
+import java.util.Scanner;
+
+public class Salary {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+
+        int openTabs = Integer.parseInt(userInput.nextLine());
+        int salary = Integer.parseInt(userInput.nextLine());
+
+        for (int i = 1; i <= openTabs; i++) {
+            String currentTab = userInput.nextLine();
+            switch (currentTab) {
+                case "Facebook":
+                    salary -= 150;
+                    break;
+                case "Instagram":
+                    salary -= 100;
+                    break;
+                case "Reddit":
+                    salary -= 50;
+                    break;
+                default:
+                    break;
+            }
+            if (salary <= 0) {
+                System.out.println("You have lost your salary.");
+                break;
+            }
+        }
+
+        if (salary > 0) {
+            System.out.println(salary);
+        }
+
+    }
+}
